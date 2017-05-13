1cf3a80f03d315da3550cff998fb3ad21fde27d5
Tyler <tylerdwarren98@gmail.com>
2017-05-13 02:27:36 -0700

    Test Commit

--- Diablo 2 Remastered/src/tech/twarren/diablo2remastered/Launcher.java
+++ Diablo 2 Remastered/src/tech/twarren/diablo2remastered/Launcher.java
@@ -3,7 +3,9 @@ package tech.twarren.diablo2remastered;
 public class Launcher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String test = "test";
+		
+		System.out.print(test);
 
 	}
 
